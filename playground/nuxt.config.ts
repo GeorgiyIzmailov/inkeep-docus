bbf8cab825c1c5a9a47309826834ef629b25bda7
GeorgiyIzmailov <georgiy-izmailov-v@inkeep.com>
2024-01-25 17:27:34 +0300

    created module

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,36 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
-  myModule: {},
-  devtools: { enabled: true }
-})
+  modules: ["../src/module"],
+  devtools: { enabled: true },
+
+  runtimeConfig: {
+    public: {
+      inkeepConfig: {
+        chatButtonType: "ICON_TEXT",
+        componentType: "SearchBar",
+        baseSettings: {
+          apiKey: "YOUR_API_KEY", // required
+          integrationId: "YOUR_INTEGRATION_ID", // required
+          organizationId: "YOUR_ORGANIZATION_ID", // required
+          primaryBrandColor: "#26D6FF", // required -- your brand color, the widget color scheme is derived from this
+          organizationDisplayName: "Inkeep",
+          // ...optional settings
+        },
+        modalSettings: {
+          // optional settings
+        },
+        searchSettings: {
+          // optional settings
+        },
+        aiChatSettings: {
+          // optional settings
+          // botAvatarSrcUrl: "/img/logo.svg", // optional -- use your own bot avatar
+          quickQuestions: [
+            "Example question 1?",
+            "Example question 2?",
+            "Example question 3?",
+          ],
+        },
+      },
+    },
+  },
+});
